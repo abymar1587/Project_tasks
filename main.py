ce5a0505e85f3516cb12c9bdab13ce40c960e6f9
abymar1587 <abymar1587@gmail.com>
2022-11-30 19:09:11 +0100

    Cambio en tittle de FatAPI

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from routes.users import users
 from routes.tasks import tasks
 
 app = FastAPI(
-    tittle="Aplicacion FastAPI con MongoDB",
+    tittle="Aplicacion FastAPI con MongoDB - Lita de tareas",
     description="Esta es una aplicacion donde se va a hacer una API REST en la que se registren usuarios y cada usuario registrado puede ingresar una lista de tareas a realizar con su titulo descripcion y sies importante o no",
 )
 
